1d6eea1603b3bfce9cce462fd25d4f6e3567ed2f
Alec Perkins <mail@alecperkins.me>
2023-01-26 17:45:54 -0500

    feat: afid() function, default to 8 characters

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,74 @@
-export default function afid () {};
+// Omits ambiguous characters (I/1/l, O/0, Z for 2, B for 8)
+const LETTERS = "ACDEFGHJKMNPQRTUVWXY";
+const NUMBERS = "2346789";
+
+/**
+ * Generate a short random identifier with affordances for human usage.
+ * Omits ambiguous characters and avoids forming words or too many
+ * sequences of one type of character.
+ * 
+ * @remarks
+ * The identifiers are not securely generated and are typically short.
+ * Do not rely on them being secret or unguessable!
+ * 
+ * @param length - (8) The character length of the identifier.
+ * 
+ * @returns A random identifier.
+ */
+function afid (length: number = 8) {
+
+  if (!length || typeof length !== "number" || length < 1) {
+    throw new Error("Length must be a positive, non-zero integer");
+  }
+
+  const picked: Array<string> = [];
+
+  let charsets: [string, string];
+  if (coinToss()) {
+    charsets = [NUMBERS, LETTERS];
+  } else {
+    charsets = [LETTERS, NUMBERS];
+  }
+
+  let num_from_set = 0;
+  let num_tries = 0;
+
+  while (picked.length < length) {
+    const current_set = charsets[0];
+    const next_char = pickRandomChar(current_set);
+    // No two of the same in a row
+    if (next_char !== picked[picked.length - 1]) {
+      picked.push(next_char);
+      num_from_set += 1;
+      // No more than 2 letters or 4 numbers in a row to avoid forming words
+      if (
+        (current_set === LETTERS && num_from_set === 2)
+        || (current_set === NUMBERS && num_from_set === 4)
+        || coinToss(0.25)
+       ) {
+        num_from_set = 0;
+        charsets.push(charsets.shift()!);
+      }
+      num_tries = 0;
+    } else if (num_tries === 10) {
+      throw new Error("Too many loops picking non-repeating characters.");
+    } else {
+      num_tries += 1;
+    }
+  }
+
+  return picked.join('');
+}
+
+function coinToss (weight = 0.5) {
+  const result = Math.random() < weight;
+  return result;
+}
+
+function pickRandomChar (charset: string) {
+  // Math.random is okay because the identifier is not a "secure" value.
+  const i = Math.floor(Math.random() * charset.length);
+  const choice = charset[i];
+  return choice;
+}
 
--- tests/afid.tests.js
+++ tests/afid.tests.js
@@ -1,3 +1,63 @@
 
 const afid = require('../dist/afid');
 
+{
+  It`should generate an identifier`;
+  const result = afid();
+  assert(result.length === 8, "Too short");
+  assert(/^[ACDEFGHJKMNPQRTUVWXY2346789]+$/.test(result), "Invalid characters");
+  assert(!/[A-Z]{3}/.test(result), "Too many sequential letters");
+  assert(!/\d{5}/.test(result), "Too many sequential numbers");
+}
+
+{
+  It`should allow customizing the length`;
+  const result = afid(12);
+  assert(result.length === 12, "Too short");
+  let error;
+  try {
+    afid(0);
+  } catch (e) {
+    error = e;
+  }
+  assert(error, "Did not throw for 0");
+  try {
+    afid(-10);
+  } catch (e) {
+    error = e;
+  }
+  assert(error, "Did not throw for negative");
+  try {
+    afid('abc');
+  } catch (e) {
+    error = e;
+  }
+  assert(error, "Did not throw for letters");
+  try {
+    afid(null);
+  } catch (e) {
+    error = e;
+  }
+  assert(error, "Did not throw for null");
+  try {
+    afid(NaN);
+  } catch (e) {
+    error = e;
+  }
+  assert(error, "Did not throw for NaN");
+}
+
+{
+  It`should throw if it takes too many loops`;
+  const realRandom = Math.random;
+  Math.random = () => 4;
+  let result;
+  let error;
+  try {
+    result = afid();
+  } catch (e) {
+    error = e;
+  }
+  assert(!result && error, "Did not throw");
+  Math.random = realRandom;
+}
