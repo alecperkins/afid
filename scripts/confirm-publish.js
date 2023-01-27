70e1d9e735c32dde59990983fff3cced780caa33
Alec Perkins <mail@alecperkins.me>
2023-01-26 19:01:01 -0500

    chore: Check release status before publishing
    Compares the local and published versions. If the same, it implies the release process has not been executed yet.

--- scripts/confirm-publish.js
+++ scripts/confirm-publish.js
@@ -1,11 +1,30 @@
 const target = require('../dist/afid/package.json');
-process.stdout.write(`\n\nReview the pack readout above, then confirm publish of ${ target.name }@${ target.version } by typing the package name:\n> `);
-process.stdin.on('data', (data) => {
-    if (data.toString().trim() === target.name) {
-        process.exit(0);
-    } else {
-        process.stdout.write(`'${ data }' does not match '${ target.name }, aborting!'`);
+
+(async () => {
+    const fetch = (await import('node-fetch')).default;
+    const npm_endpoint = `https://registry.npmjs.com/${ target.name }`;
+    const published_package = await (await fetch(npm_endpoint)).json();
+    const latest_version = published_package["dist-tags"].latest;
+    process.stdout.write(`Local version: ${ target.version }\n`);
+    process.stdout.write(`Published version: ${ latest_version }\n`);
+
+    if (latest_version === target.version) {
+        process.stdout.write(`\nLocal and published versions are the same (${ latest_version }).
+Run \`npm run release-[major,minor,patch]\` first!\n`);
         process.exit(1);
     }
-});
 
+    process.stdout.write(`\n\nReview the pack readout above, then confirm publish of ${ target.name }@${ target.version } by typing the package name:\n> `);
+    process.stdin.on('data', (data) => {
+        if (data.toString().trim() === target.name) {
+            process.exit(0);
+        } else {
+            process.stdout.write(`'${ data }' does not match '${ target.name }, aborting!'`);
+            process.exit(1);
+        }
+    });
+
+})().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
